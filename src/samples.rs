0bd0a2ecee071ef18c75d783c41e6de291084fb8
Enzo Cioppettini <git@enzocioppettini.com>
2023-05-28 22:23:20 -0300

    add bulk sample loading

---
+++ src/samples.rs
@@ -0,0 +1,183 @@
+use crate::BLOCK_SIZE;
+use anyhow::Context;
+use glicol::Engine;
+use rayon::prelude::*;
+use std::{fs::File, path::Path};
+use symphonia::core::{
+    audio::Signal, codecs::DecoderOptions, formats::FormatReader, io::MediaSourceStream,
+    probe::Hint,
+};
+
+pub fn load_samples_from_env(engine: &mut Engine<BLOCK_SIZE>) {
+    let key = "GLICOL_CLI_SAMPLES_PATH";
+
+    if let Some(paths) = std::env::var_os(key) {
+        for path in std::env::split_paths(&paths) {
+            if let Err(error) = load_samples_from_dir(engine, &path) {
+                eprintln!("failed to load samples from {:?}, reason: {}", path, error);
+            }
+        }
+    }
+}
+
+fn load_samples_from_dir(
+    engine: &mut Engine<BLOCK_SIZE>,
+    dir: impl AsRef<Path>,
+) -> anyhow::Result<()> {
+    let walk_dir = walkdir::WalkDir::new(dir)
+        .min_depth(1)
+        .max_depth(2)
+        .into_iter()
+        .filter_entry(|entry| {
+            entry
+                .file_name()
+                .to_str()
+                .map(|s| !s.starts_with('.'))
+                .unwrap_or(false)
+        })
+        .filter_map(|entry| {
+            entry.ok().filter(|entry| {
+                AsRef::<std::path::Path>::as_ref(&entry.file_name())
+                    .extension()
+                    .map(|ext| ext == "wav")
+                    .unwrap_or(false)
+            })
+        })
+        .map(|entry| (entry.path().to_path_buf(), entry.depth()))
+        .collect::<Vec<_>>();
+
+    for (sample, name) in walk_dir
+        .par_iter()
+        .filter_map(|(path, depth)| {
+            let prefix = if *depth == 2 {
+                path.parent()
+                    .unwrap()
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+            } else {
+                ""
+            };
+
+            let name = format!(
+                "\\{}{}",
+                prefix,
+                path.file_stem().unwrap().to_str().unwrap()
+            );
+
+            load_sample(path).ok().zip(Some(name))
+        })
+        .collect::<Vec<_>>()
+    {
+        let sample_buffer = Box::leak(sample.buffer.into_boxed_slice());
+        engine.add_sample(&name, sample_buffer, sample.channels, sample.sr);
+    }
+
+    Ok(())
+}
+
+struct Sample {
+    buffer: Vec<f32>,
+    sr: usize,
+    channels: usize,
+}
+
+fn load_sample(path: impl AsRef<Path>) -> anyhow::Result<Sample> {
+    let mut hint = Hint::new();
+
+    if let Some(extension) = path.as_ref().extension() {
+        if let Some(extension_str) = extension.to_str() {
+            hint.with_extension(extension_str);
+        }
+    }
+
+    let source = Box::new(File::open(path)?);
+
+    let mss = MediaSourceStream::new(source, Default::default());
+
+    match symphonia::default::get_probe().format(
+        &hint,
+        mss,
+        &Default::default(),
+        &Default::default(),
+    ) {
+        Ok(probed) => decode(probed.format, &DecoderOptions { verify: false })
+            .context("Couldn't decode sample"),
+        Err(err) => {
+            anyhow::bail!("input format not supported: {err}");
+        }
+    }
+}
+
+fn decode(
+    mut reader: Box<dyn FormatReader>,
+    decode_opts: &DecoderOptions,
+) -> anyhow::Result<Sample> {
+    let track = reader.default_track().unwrap();
+    let track_id = track.id;
+
+    let sr = track
+        .codec_params
+        .sample_rate
+        .ok_or(anyhow::anyhow!("Couldn't get sample rate"))? as usize;
+
+    let mut decoder = symphonia::default::get_codecs().make(&track.codec_params, decode_opts)?;
+
+    let channels = track
+        .codec_params
+        .channels
+        .ok_or(anyhow::anyhow!("Couldn't get channel info"))?
+        .count();
+
+    if channels > 2 {
+        anyhow::bail!("unsupported channel number");
+    }
+
+    let mut channel_0: Vec<f32> = vec![];
+    let mut channel_1: Vec<f32> = vec![];
+
+    let result = loop {
+        let packet = match reader.next_packet() {
+            Ok(packet) => packet,
+            Err(err) => break Err(err),
+        };
+
+        if packet.track_id() != track_id {
+            continue;
+        }
+
+        let decoded = match decoder.decode(&packet) {
+            Ok(decoded) => decoded,
+            Err(err) => break Err(err),
+        };
+
+        let mut buffer = decoded.make_equivalent::<f32>();
+
+        decoded.convert(&mut buffer);
+        channel_0.extend(buffer.chan(0));
+
+        if channels == 2 {
+            channel_1.extend(buffer.chan(1));
+        }
+    };
+
+    match result {
+        Err(symphonia::core::errors::Error::IoError(err))
+            if err.kind() == std::io::ErrorKind::UnexpectedEof
+                && err.to_string() == "end of stream" =>
+        {
+            Ok(())
+        }
+        _ => result,
+    }?;
+
+    // if mono then channel_1 would be empty, so no change here
+    channel_0.extend(channel_1);
+
+    Ok(Sample {
+        buffer: channel_0,
+        sr,
+        channels,
+    })
+}
